3e9e632fa4b6cae69e00c37bd98c38f1212fad6d
Sinnott74 <sinnott74@hotmail.com>
2016-04-13 17:50:41 +0100

    Added comments to functions

--- script/Player.js
+++ script/Player.js
@@ -1,3 +1,22 @@
+/**
+ * Constructor for Player. A player has contol over a single paddle in the game
+ * 
+ * @param {number}
+ *            x The x coordinate of the upper left point of the player's paddle
+ * @param {number}
+ *            y The y coordinate of the upper left point of the player's paddle
+ * @param {number}
+ *            width The width of the player's paddle
+ * @param {number}
+ *            height The height of the player's paddle
+ * @param {number}
+ *            movementDistance The distance a player can move in each frame.
+ *            Also know as player speed
+ * @param {number}
+ *            leftKey The keyCode value of the key which moves the paddle left
+ * @param {number}
+ *            rightKey The keyCode value of the key which moves the paddle right
+ */
 function Player(x, y, width, height, movementDistance, leftKey, rightKey) {
 
 	this.x = x;
@@ -14,6 +33,12 @@ function Player(x, y, width, height, movementDistance, leftKey, rightKey) {
 	this.score = 0;
 }
 
+/**
+ * Checks if the player's left or right keys have been pressed and updates the
+ * coordinates of the player but the movementDistance
+ * 
+ * @this {Player}
+ */
 Player.prototype.move = function() {
 	if (keys[this.upKey]) {
 		if (this.y > 0) {
@@ -28,6 +53,11 @@ Player.prototype.move = function() {
 	}
 }
 
+/**
+ * Draws the player's paddle on the screen
+ * 
+ * @this {Player}
+ */
 Player.prototype.draw = function() {
 	ctx.beginPath();
 	ctx.rect(this.x, this.y, this.width, this.height);

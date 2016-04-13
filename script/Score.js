84acf7c094764950543fd0cbccc27cda9b1c2a6b
Sinnott74 <sinnott74@hotmail.com>
2016-04-13 17:53:20 +0100

    Added comments

--- script/Score.js
+++ script/Score.js
@@ -1,9 +1,22 @@
+/**
+ * Constructor for a Ball
+ * 
+ * @param {number}
+ *            x The x coordinate of the score's centre point
+ * @param {number}
+ *            y The y coordinate of the score's centre point
+ * @param {Array}
+ *            playerList An array of player to include in the score
+ */
 function Score(x, y, playerList) {
 	this.x = x;
 	this.y = y;
 	this.playerList = playerList;
 }
 
+/**
+ * Draws the Score of each player onto the screen as text
+ */
 Score.prototype.draw = function() {
 	
 	var scoreString

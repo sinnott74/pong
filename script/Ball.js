fa2c5e532903679a3e7b56336114c93fb10991ed
Sinnott74 <sinnott74@hotmail.com>
2016-04-13 17:50:20 +0100

    Added comments to functions

--- script/Ball.js
+++ script/Ball.js
@@ -1,3 +1,17 @@
+/**
+ * Constructor for a Ball
+ * 
+ * @param {number}
+ *            x The x coordinate of the ball's centre point
+ * @param {number}
+ *            y The y coordinate of the ball's centre point
+ * @param {number}
+ *            dx The velocity of the ball in the x direction
+ * @param {number}
+ *            dy The velocity of the ball in the y direction
+ * @param {number}
+ *            radius The radius of the ball
+ */
 function Ball(x, y, dx, dy, radius) {
 	this.x = x;
 	this.y = y;
@@ -6,6 +20,12 @@ function Ball(x, y, dx, dy, radius) {
 	this.radius = radius;
 }
 
+/**
+ * Checks to see if the ball has collided with a wall or player's paddle. Then
+ * updates the balls position based on its velocity
+ * 
+ * @this {Ball}
+ */
 Ball.prototype.move = function() {
 	if (this._checkXWallCollision() || playerCollision()) {
 		this.dx = -this.dx;
@@ -23,33 +43,56 @@ Ball.prototype.move = function() {
 
 }
 
+/**
+ * Checks if the ball has collided with a wall in the X plane. Plays sound if
+ * true
+ * 
+ * @returns {Boolean} true if the ball has collided with a wall, otherwise false
+ */
 Ball.prototype._checkXWallCollision = function() {
 	if (this.x > CANVAS_WIDTH - this.radius || this.x < this.radius) {
-		
-		if(WALL_SOUND){
-			WALL_SOUND.currentTime = 0;
-			WALL_SOUND.play();
-		}
+
+		this.playSound();
+
 		return true;
 	}
 	return false;
 
 }
 
+/**
+ * Checks if the ball has collided with a wall in the Y plane. Plays sound if
+ * true
+ * 
+ * @returns {Boolean} true if the ball has collided with a wall, otherwise false
+ */
 Ball.prototype._checkYWallCollision = function() {
 	if (this.y > CANVAS_HEIGHT - this.radius || this.y < this.radius) {
-		
-		if(WALL_SOUND){
-			WALL_SOUND.currentTime = 0;
-			WALL_SOUND.play();
-		}
-		
+
+		this.playSound();
+
 		return true;
 	}
 	return false;
 
 }
 
+/**
+ * Plays the balls sound if it is available to be played
+ * 
+ * @this {Ball}
+ */
+Ball.prototype.playSound = function() {
+	if (WALL_SOUND) {
+		WALL_SOUND.currentTime = 0;
+		WALL_SOUND.play();
+	}
+}
+/**
+ * Draws the ball on the screen
+ * 
+ * @this {Ball}
+ */
 Ball.prototype.draw = function() {
 
 	ctx.beginPath();
